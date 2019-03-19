b77bdb1ef2c39dc5df100c6b5b6b2207fd20e799
phalikid <48722656+phalikid@users.noreply.github.com>
2019-03-19 23:45:46 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -38,7 +38,8 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $result $content . "\r\n";
+echo $result. "\r\n";
+echo $content."\r\n";
 }
 }
 }
