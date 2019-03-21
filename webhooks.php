65a9b11b917bc3c96a3c57598fb5ff8bdd77ef74
phalikid <48722656+phalikid@users.noreply.github.com>
2019-03-21 18:59:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -47,8 +47,8 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $result. "\r\n";
-echo "https://psis.in.th/reg_linebot.php?idpush=".$result." \r\n";
+//echo $result. "\r\n";
+echo "https://psis.in.th/reg_linebot.php?idpush= \r\n";
 }
 }
 }
