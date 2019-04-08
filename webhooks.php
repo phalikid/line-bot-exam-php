1ee64c61f7c1ecc81326aeb07019617f00c85d7c
phalikid <48722656+phalikid@users.noreply.github.com>
2019-04-08 14:13:58 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ foreach ($events['events'] as $event) {
 // Reply only when message sent is in 'text' format
 if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
-$text = "https://psis.in.th/reg_linebot.php?idpush=".$event['source']['userId']."";
+$text = "http://psis.in.th/reg_linebot.php?idpush=".$event['source']['userId']."";
 // Get replyToken
 $replyToken = $event['replyToken'];
 // Build message to reply back
