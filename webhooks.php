b6a50a852de5301539dfbe1424415cb146c360e6
phalikid <48722656+phalikid@users.noreply.github.com>
2019-04-18 12:50:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -22,7 +22,7 @@ if (!is_null($events['events'])) {
 foreach ($events['events'] as $event) {
 // Reply only when message sent is in 'text' format
 if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-if($message=="id"){
+if(trim($message)=="id"){
 // Get text sent
 $text = "http://psis.in.th/reg_linebot.php?idpush=".$event['source']['userId']."&idaccess=".$access_token." ";
 // Get replyToken
@@ -135,6 +135,32 @@ if($message=="4"){
 	$messages = [
 	'type' => 'text',
 	'text' => "เว็บไซต์โรงเรียน http://www.nyp.ac.th",
+	];
+	// Make a POST Request to Messaging API to reply to sender
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	$data = [
+	'replyToken' => $replyToken,
+	'messages' => [$messages],
+	];
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	echo $result. "\r\n";
+}
+if($message=="5"){
+	// Get replyToken
+	$replyToken = $event['replyToken'];
+	// Build message to reply back
+	$messages = [
+	'type' => 'text',
+	'text' => "กรุณาคลิกเพื่อเข้าติดต่อ https://www.facebook.com/wearenyp/inbox/?mailbox_id=485449128176988&selected_item_id=100016303413355",
 	];
 	// Make a POST Request to Messaging API to reply to sender
 	$url = 'https://api.line.me/v2/bot/message/reply';
