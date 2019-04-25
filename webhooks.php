58bbd7c02c39352e9f06e25693d65a0263bbc622
phalikid <48722656+phalikid@users.noreply.github.com>
2019-04-25 22:02:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -83,25 +83,17 @@ if(trim($message)=="รายงานการสแกนบัตร"){
 	// Get replyToken
 	$replyToken = $event['replyToken'];
 	// Build message to reply back
-	$messages =  [
-    {
-      "type": "flex",
-      "altText": "This is a Flex Message",
-      "contents": {
-        "type": "bubble",
-        "body": {
-          "type": "box",
-          "layout": "horizontal",
-          "contents": [
-            {
-              "type": "text",
-              "text": "Hello,"
-            },
-            {
-              "type": "text",
-              "text": "World!"
-            }
-          ];
+	$messages = [
+	'type' => 'text',
+	'text' => "http://www.psis.in.th/report_print/std_ma.php?idpush=".$event['source']['userId']."",
+	];
+	// Make a POST Request to Messaging API to reply to sender
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	$data = [
+	'replyToken' => $replyToken,
+	'messages' => [$messages],
+	];
+
 	// Make a POST Request to Messaging API to reply to sender
 	$url = 'https://api.line.me/v2/bot/message/reply';
 	$data = [
